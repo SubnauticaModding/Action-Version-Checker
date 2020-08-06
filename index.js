cd0f4452b52be19e77ea9a32eae6bd12b38cdb84
AlexejheroYTB <alexejhero@gmail.com>
2020-08-06 17:31:52 +0300

    master branch check

--- index.js
+++ index.js
@@ -3,32 +3,71 @@ const github = require("@actions/github");
 
 const fs = require("fs");
 const MultiRegExp2 = require("multi-regexp2").default;
-const { start } = require("repl");
+const path = require("path");
 
 try {
-  console.log("Examining files...");
+  console.log("Examining PR branch files...");
 
-  const text = core.getInput("files");
+  const text = core.getInput("files", { required: true });
   const dictionary = getDictionary(text);
   const annotations = getAnnotations(dictionary);
   const versions = annotations.map(x => x.text);
   addCount(annotations, versions);
 
-  if (versions.length == 0) return;
+  if (versions.length == 0) {
+    console.log("No versions found on PR branch.");
+    return;
+  }
+
+  core.setOutput("annotations_path", "./annotations.json");
+  core.setOutput("failed_check_path", "./failed.txt");
 
   if (versions.filter(x => x == versions[0]).length != versions.length) {
-    console.log("The version numbers don't match!");
+    console.log("The version numbers on the PR branch don't match!");
     console.log(versions);
     console.log(annotations);
     fs.writeFileSync("./annotations.json", JSON.stringify(annotations));
     fs.writeFileSync("./failed.txt", "1");
+    return;
   } else {
-    console.log("The version numbers match.");
+    console.log("The version numbers on the PR branch match.");
     console.log(versions);
     fs.writeFileSync("./annotations.json", "[]");
   }
 
-  // TODO: Make sure version number isn't the same as master
+  console.log("Examining master branch files...");
+
+  const masterPath = core.getInput("master_repo_path");
+  if (!masterPath) {
+    console.log("No master branch path set. Skipping master check.");
+    return;
+  }
+
+  const masterAnnotations = getAnnotations(dictionary, masterPath);
+  const masterVersions = masterAnnotations.map(x => x.text);
+
+  if (masterVersions.length == 0) {
+    console.log("No versions found on master branch. Skipping master check.");
+    return;
+  }
+
+  if (masterVersions.filter(x => x == masterVersions[0]).length != masterVersions.length) {
+    console.log("The version numbers on the master branch don't match! Skipping master check.");
+    return;
+  }
+
+  if (versions[0] == masterVersions[0]) {
+    console.log("PR branch version is the same as master branch version!");
+    console.log(versions[0]);
+    setSameAsMaster(annotations, versions[0]);
+    fs.writeFileSync("./annotations.json", JSON.stringify(annotations));
+    fs.writeFileSync("./failed.txt", "1");
+    return;
+  } else {
+    console.log("PR branch version is not the same as the master branch version.");
+    console.log(`PR: ${versions[0]}, MASTER: ${masterVersions[0]}`);
+    fs.writeFileSync("./annotations.json", "[]");
+  }
 } catch (error) {
   core.setFailed("An error has occurred: " + error.message);
 }
@@ -61,12 +100,13 @@ function getDictionary(text) {
 /**
  * This function turns the dictionary returned by `getDictionary(string)` into an array of annotations which can be understood by the `Attest/annotations-action` action
  * @param {{[file: string]: string}} dict 
- * @returns {{message: "This version number doesn't match other ones.", path: string, column: {start: number, end: number}, line: {start: number, end: number}, level: "failure", text: string}[]}
+ * @param {string} _path
+ * @returns {{message: string, path: string, column: {start: number, end: number}, line: {start: number, end: number}, level: "failure", text: string}[]}
  */
-function getAnnotations(dict) {
+function getAnnotations(dict, _path = ".") {
   const output = [];
   for (const file in dict) { // For each file in the dictionary
-    const contents = fs.readFileSync("./" + file, "utf-8"); // Read file's contents
+    const contents = fs.readFileSync(path.join(_path, file), "utf-8"); // Read file's contents
     contents.split(/[\n\r]+/g).forEach((line, index) => { // For each line in the file
       for (const regexText of dict[file]) { // For each regex in the dictionary
         /** @type {RegExp} */
@@ -102,7 +142,7 @@ function getAnnotations(dict) {
 
 /**
  * This function appends the number of occurrences of each version to the message of the annotations
- * @param {{message: "This version number doesn't match other ones.", path: string, column: {start: number, end: number}, line: {start: number, end: number}, level: "failure", text: string}[]} annotations 
+ * @param {{message: string, path: string, column: {start: number, end: number}, line: {start: number, end: number}, level: "failure", text: string}[]} annotations 
  * @param {string[]} versions 
  */
 function addCount(annotations, versions) {
@@ -116,5 +156,16 @@ function addCount(annotations, versions) {
     for (const version in count) {
       annotation.message += `\n- ${count[version]} occurrence${count[version] == 1 ? "" : "s"} of "${version}"`; // TODO: Sort this with a custom version comparer or by count
     }
+  }
+}
+
+/**
+ * This function changes the message of the annotations to specify that the version is the same as the master version
+ * @param {{message: string, path: string, column: {start: number, end: number}, line: {start: number, end: number}, level: "failure", text: string}[]} annotations 
+ * @param {string} version 
+ */
+function setSameAsMaster(annotations, version) {
+  for (const annotation of annotations) {
+    annotation.message = `The version number is the same as the master branch! (${version})`;
   }
 }
